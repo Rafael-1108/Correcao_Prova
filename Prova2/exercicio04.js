0ed11444049114d66d924166f43357511e93dfc9
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-05-22 18:22:36 -0300

    atv04

---
+++ Prova2/exercicio04.js
@@ -0,0 +1,11 @@
+let numeros = [5, 2, 3, 2, 4];
+let soma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+   console.log('soma:', soma);
+   console.log('numero:', numeros[i]);
+    soma = soma + numeros[i];
+}
+console.log(soma);
+
+// 1 ponto
