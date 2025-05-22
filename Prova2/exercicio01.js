2074947c0357fed0f31d3d16c1154c4d5f99a8b0
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-05-22 18:37:01 -0300

    atv01

---
+++ Prova2/exercicio01.js
@@ -0,0 +1,10 @@
+const valorMesada = 150;
+
+let gasto = 75;
+let saldo = valorMesada - gasto;
+
+console.log("Exercicio 01");
+console.log("Valor restante :" + valorMesada);
+console.log("-------------------------------------");
+
+//0.5 ponto
