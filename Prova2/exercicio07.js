0e140d72ef8a550721d1c5107191d357da0bb288
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-05-22 18:17:59 -0300

    atv07

---
+++ Prova2/exercicio07.js
@@ -0,0 +1,26 @@
+let pedido = 3;
+let nomeCombo = "";
+let preco = 0
+
+switch(pedido){
+    case 1:
+        nomeCombo = "Batata + Suco";
+        preco = 12;
+        break;
+    case 2:
+        nomeCombo = "Hamburguer";
+        preco = 15;
+        break;
+    case 3:
+        nomeCombo = "Pizza Broto";
+        preco = 20;
+        break;
+    default:
+        console.log("Opção invalida");
+}
+if(nomeCombo != ""){
+    console.log("combo escolhido", nomeCombo );
+    console.log("Preço: R$", preco);
+}
+
+//  1 ponto
