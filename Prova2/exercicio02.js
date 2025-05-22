f4c9b3c267374c287fff6fb9e86cbe7ff153b606
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-05-22 18:25:29 -0300

    atv02

---
+++ Prova2/exercicio02.js
@@ -0,0 +1,13 @@
+let pontuacao = 71;
+
+console.log("Exercicio 02");
+if(pontuacao < 40){
+    console.log("Eliminado!!!");
+}else if(pontuacao <= 70){
+    console.log("Classificaso! ");
+}else{
+    console.log("Classificado para a final!");
+}
+console.log("------------------------------------");
+
+//1 ponto
