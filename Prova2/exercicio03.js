4e28ad33a6af21b0e6b38d06e5122e2b0e5f5823
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-05-22 18:23:42 -0300

    atv03

---
+++ Prova2/exercicio03.js
@@ -0,0 +1,17 @@
+let opçao = 2;
+
+switch(opçao) {
+    case 1:
+        console.log("Você escolheu cadastrar produtos");
+        break;
+    case 2:
+        console.log("Você escolheu listar produtos");
+        break;
+    case 3:
+        console.log("Você escolheu sair");
+        break;
+    default:
+        console.log("opção Invalida!");
+}
+
+// 1 ponto
